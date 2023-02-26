5d6eb7bbf1e777af56db3f1b738121dc252d605a
akaSomix <somix.land@null.net>
2023-02-26 21:25:51 +0200

    feat: ugly navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    Navbar
-  </div>
+  <nav class="flex justify-between p-2 px-4 bg-cyan-800">
+    <!-- Left Block -->
+    <div class="grid grid-flow-col gap-4 content-center">
+      <div class="col-span-2 font-bold text-xl">
+        Events Gallery 🎞️
+      </div>
+      
+      <NuxtLink to="/events" >
+        Events
+      </NuxtLink>
+      
+      <NuxtLink to="/about">
+        About
+      </NuxtLink>
+    </div>
+
+    <!-- Right Block -->
+    <div>
+      <div>🧑</div>
+    </div>
+  </nav>
 </template>
