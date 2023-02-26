19fd7925fac2a7dc8879901feb9f74306a42b8bb
akaSomix <somix.land@null.net>
2023-02-26 08:45:14 +0200

    feat: add husky configuration

--- app.vue
+++ app.vue
@@ -1,5 +1,29 @@
+<script lang="ts" setup>
+
+useHead({
+  title: 'Events Gallery',
+  titleTemplate: '%s - Events Gallery',
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      hid: 'test',
+      name: 'test',
+      content: 'Events Gallery',
+    },
+  ],
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+})
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Html>
+    <Body
+      class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900"
+    >
+      <NuxtLayout>
+        <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
+        <NuxtPage />
+      </NuxtLayout>
+    </Body>
+  </Html>
 </template>
